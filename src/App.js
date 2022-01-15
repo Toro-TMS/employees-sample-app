3d79a0f3fdaddaf86549670cc162a70ecd19d3b9
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-01-14 23:07:31 -0500

    server tests

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 import { makeServer } from "./server";
+//import { useEffect } from "react";
+import Employees from "./components/Employees";
+import { useState, useEffect } from "react/cjs/react.development";
 
 if (process.env.NODE_ENV === "development") {
   makeServer({ environment: "development" });
 }
 
 function App() {
+  const [employees, setEmployees] = useState([])
+   useEffect(() => {
+     async function fetchEmployees() {
+       try {
+         const response = makeServer()
+         console.log(response.environment, "👹")
+         setEmployees(response.seeds)
+       } catch(err) {
+         console.log(`${err} 👾`)
+       }
+     }
+     fetchEmployees()
+   }, [])
   return (
     <div>
       <header>
         <h1>Employees</h1>
       </header>
+      <Employees  info={employees}/>
     </div>
   );
 }
---
+++ src/components/Employees.js
@@ -0,0 +1,10 @@
+
+export default function Employees(props) {
+    console.log(props, "🐼")
+    return(
+        <div>
+            <p>one Employee</p>
+
+        </div>
+    )
+}
