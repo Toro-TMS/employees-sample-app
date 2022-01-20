d1357df7b6f241079496f7a6b7255ab12166eb5a
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-01-20 16:29:43 -0500

    remove employee from page

--- src/components/Employee.js
+++ src/components/Employee.js
@@ -1,12 +1,17 @@
 import Info from "./Info"
 import Delete from "./Delete"
+import { useState } from "react"
 
 // TODO create expand button for details div
 export default function Employee(props){
+    const [open, setOpen] = useState(true)
    //console.log(props.employee, "👾")
+   const hideEmployee = (e) => {
+    console.log(e, "is deleted")
+}
    const employeeList = props.employee?.map((employeeData, index) => {
         return(
-            <li key = {index}>
+            <li key = {index} >
                 <img className="avatar" src={employeeData.avatar} alt={employeeData.firstName} width="200" height="150"/>
                 <b>{employeeData.firstName} {employeeData.lastName}</b> - {employeeData.email}
                 <div className="details">
@@ -14,10 +19,11 @@ export default function Employee(props){
                     {employeeData.bio}
                 <Info address={employeeData.address} id={employeeData.id}/>
                 </div>
-                <Delete id={employeeData.id}/>
+                <Delete id={employeeData.id} onClick={hideEmployee}/>
              </li>
          )
         })
+    
         return(
             <div>
         {employeeList}  
