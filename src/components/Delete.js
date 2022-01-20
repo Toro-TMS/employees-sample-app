2f44ee3b10bfb50a5dd92b63b4ab282c4acf2335
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-01-20 01:06:11 -0500

    incomplete delete

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -1,4 +1,13 @@
+import { useState } from "react"
+
 export default function Delete() {
+    const [employeeInfo, setEmployeeInfo] = useState([])
+    const handleDelete = (e) => {
+        //DELETE employees/:id
+        // async function deleteEvent() {
+        // }
+        setEmployeeInfo([])
+    }
     return(
         <div>
             🗑
