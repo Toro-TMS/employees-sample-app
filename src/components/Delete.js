b5be5106daf46b0b92f0953b51974ca98704a52a
lizlukasiewicz <lizlukasiewicz@yahoo.com>
2022-01-21 18:25:39 -0500

    backup before extra credit

--- src/components/Delete.js
+++ src/components/Delete.js
@@ -3,14 +3,13 @@ import { Button } from "@material-ui/core"
 
 // DELETES employee on the back end, needs to be reflected/page refresh on the front end
 export default function Delete(props) {
-    console.log(props, "❌")
+    //console.log(props, "❌")
     //const [employeeInfo, setEmployeeInfo] = useState([])
     const handleDelete = (e) => {
         fetch(`api/employees/${props.employeeId}`, {method: 'DELETE'})
         .then((response) => response.json())
         .then((response) => console.log(response))
         .catch((error) => console.log(error))
-    
     }
     return(
         <Button onClick={handleDelete}>
